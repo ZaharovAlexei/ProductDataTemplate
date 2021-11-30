82a528c31e9e0f01e71b4d87cf16f3351dc621ce
Алексей Захаров <zakharov.nnov@gmail.com>
2021-12-01 00:41:07 +0300

    Добавьте файлы проекта.

---
+++ ProductDataTemplate/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductDataTemplate
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new List<Product>();
+            products.Add(new Product()
+            {
+                NameProduct = "Apples",
+                Price = 99,
+                ImageProduct = "Data/apple.jpg",
+                Category = Categories.Meal
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Bananas",
+                Price = 69,
+                ImageProduct = "Data/banana.jpg",
+                Category = Categories.Meal
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Hoover",
+                Price = 4999,
+                ImageProduct = "Data/hoover.jpg",
+                Category = Categories.Appliances
+            });
+            lstBox.ItemsSource = products;
+        }
+    }
+}
---
+++ ProductDataTemplate/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductDataTemplate
+{
+    public enum Categories
+    {
+        Meal,
+        Appliances
+    }
+    public class Product
+    {
+        public string NameProduct { get; set; }
+        public int Price { get; set; }
+        public string ImageProduct { get; set; }
+        public Categories Category { get; set; }
+    }
+}
